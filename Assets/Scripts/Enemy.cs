fb6fd727374326e81d4b812996e7e64bc8f482e0
darianny08 <dariannyo@nycstudents.net>
2025-01-13 12:18:48 -0500

    completed 4.4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,10 +17,16 @@ public class Enemy : MonoBehaviour
 
 
     
-    // Update is called once per frame
+     // Update is called once per frame
     void Update()
     {
-        Vector3 LookDirection = (player.transform.position-transform.position).normalized;
-        enemyRb.AddForce(LookDirection * speed);
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+		enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10) 
+        {
+            Destroy (gameObject);
+        }
     }
+
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,25 +5,48 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    private float spawnRange = 9;
+    private float spawnRange = 3;
+    public GameObject PowerupPrefab;
+    public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-      Instantiate (enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+      SpawnEnemyWave(waveNumber);
+      Instantiate (PowerupPrefab, GenerateSpawnPosition(), PowerupPrefab.transform.rotation);
+
     }
 
+    void Update()
+    {
+       enemyCount = FindObjectsOfType<Enemy>().Length;
+      if(enemyCount == 0) 
+      { 
+        waveNumber++; 
+        SpawnEnemyWave( waveNumber);
+        Instantiate (PowerupPrefab, GenerateSpawnPosition(), PowerupPrefab.transform.rotation);
+      }
+    }
+  void SpawnEnemyWave(int enemiesToSpawn)
+  {
+    for (int i = 0; i < enemiesToSpawn; i++)
+    {
+        Instantiate (enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
+
+    }
+
+  }
     private Vector3 GenerateSpawnPosition()
     {
-        float spawnPosX = Random.Range(spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(spawnRange, spawnRange);
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
+
+   
 }
